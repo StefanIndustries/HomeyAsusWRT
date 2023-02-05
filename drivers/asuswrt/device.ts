06d99ebbcc6cd0dec46213896be1fdc194c021b2
Stefan <sjsimon1996@gmail.com>
2023-02-05 16:31:00 +0100

    Added new device and repair functionality.

--- drivers/asuswrt/device.ts
+++ drivers/asuswrt/device.ts
@@ -1,19 +1,62 @@
 import Homey from 'homey';
+import { AsusWRTOperationMode } from 'node-asuswrt/lib/models/AsusWRTOperationMode';
 
 class AsusWRTDevice extends Homey.Device {
+
+  static routerCapabilities = [
+    'alarm_wan_disconnected',
+    'external_ip',
+    'meter_cpu_usage',
+    'meter_mem_used',
+    'meter_online_devices',
+    'traffic_total_received',
+    'traffic_total_sent',
+  ];
+
+  static accessPointCapabilities = [
+    'meter_cpu_usage',
+    'meter_mem_used',
+    'meter_online_devices',
+  ];
+
+  private async wait(milliseconds: number) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+  }
+
+  private async setCapabilities(operationMode: AsusWRTOperationMode) {
+    const capabilityList = operationMode === AsusWRTOperationMode.Router ? AsusWRTDevice.routerCapabilities : AsusWRTDevice.accessPointCapabilities;
+    capabilityList.forEach(async cap => {
+      this.log(`test capability: ${cap} set for device`);
+      if (!this.hasCapability(cap)) {
+        await this.wait(1000);
+        this.log(`capability: ${cap} for device missing, adding now.`);
+        await this.addCapability(cap);
+        if (!this.hasCapability(cap)) {
+          await this.wait(10000);
+          this.log(`capability: ${cap} for device still missing, adding now.`);
+          await this.addCapability(cap);
+        }
+      } else
+      {
+        this.log(`capability: ${cap} already available for device.`)
+      }
+    });
+  }
 
   /**
    * onInit is called when the device is initialized.
    */
   async onInit() {
-    this.log('MyDevice has been initialized');
+    await this.setCapabilities(this.getStoreValue('operationMode'));
+    this.log('AsusWRTDevice has been initialized');
   }
 
   /**
    * onAdded is called when the user adds the device, called just after pairing.
    */
   async onAdded() {
-    this.log('MyDevice has been added');
+    await this.setCapabilities(this.getStoreValue('operationMode'));
+    this.log('AsusWRTDevice has been added');
   }
 
   /**
@@ -25,7 +68,7 @@ class AsusWRTDevice extends Homey.Device {
    * @returns {Promise<string|void>} return a custom message that will be displayed
    */
   async onSettings({ oldSettings: {}, newSettings: {}, changedKeys: [] }): Promise<string|void> {
-    this.log('MyDevice settings where changed');
+    this.log('AsusWRTDevice settings where changed');
   }
 
   /**
@@ -34,14 +77,14 @@ class AsusWRTDevice extends Homey.Device {
    * @param {string} name The new name
    */
   async onRenamed(name: string) {
-    this.log('MyDevice was renamed');
+    this.log('AsusWRTDevice was renamed');
   }
 
   /**
    * onDeleted is called when the user deleted the device.
    */
   async onDeleted() {
-    this.log('MyDevice has been deleted');
+    this.log('AsusWRTDevice has been deleted');
   }
 
 }
--- drivers/asuswrt/driver.ts
+++ drivers/asuswrt/driver.ts
@@ -1,6 +1,6 @@
 import Homey from 'homey';
 import PairSession from 'homey/lib/PairSession';
-import { AsusWRT } from "node-asuswrt";
+import { AsusWRT } from "node-asuswrt"
 
 class AsusWRTDriver extends Homey.Driver {
 
@@ -25,11 +25,9 @@ class AsusWRTDriver extends Homey.Driver {
         password = this.homey.settings.get('password');
         if (routerIP !== null && username !== null && password !== null) {
           client = new AsusWRT(routerIP, username, password);
-          const loginSuccess = await client.login().catch(error => {
-            this.log(error);
-          });
+          const routers = await client.getRouters();
           client.dispose();
-          if (loginSuccess) {
+          if (routers.length > 0) {
             await session.showView('list_devices');
           } else {
             await session.showView('router_ip');
@@ -60,28 +58,34 @@ class AsusWRTDriver extends Homey.Driver {
       username = data.username.trim();
       password = data.password;
       this.log('creating client');
-      client = new AsusWRT(routerIP, username, password);
-      const loginSuccess = await client.login().catch(error => {
-        this.log('failed to login');
-        this.log(error);
-        return Promise.reject(Error('Failed to login'));
-      });
+      let routers = [];
+      try {
+        client = new AsusWRT(routerIP, username, password);
+        routers = await client.getRouters();
+      } catch {
+        return false;
+      }
       client.dispose();
-      if (loginSuccess) {
+      if (routers.length === 0) {
+        this.log('failed to login');
+        return Promise.reject(Error('Failed to login'));
+      } else {
         this.homey.settings.set("ip", routerIP);
         this.homey.settings.set("username", username);
         this.homey.settings.set("password", password);
+        return true;
       }
-      return loginSuccess;
     });
 
     session.setHandler('list_devices', async () => {
       this.log('pair: list_devices');
       client = new AsusWRT(this.homey.settings.get('ip'), this.homey.settings.get('username'), this.homey.settings.get('password'));
-      await client.login();
-      const routerAPDevices = await client.getRouters().catch(error => Promise.reject(error));
+      const routerAPDevices = await client.getRouters().catch(error => {
+        session.showView('router_ip');
+        return;
+      });
       client.dispose();
-      const foundDevices = routerAPDevices.map(device => {
+      const foundDevices = routerAPDevices!.map(device => {
         return {
           name: `${device.productId} ${device.alias}`,
           data: {
@@ -96,6 +100,52 @@ class AsusWRTDriver extends Homey.Driver {
       return foundDevices;
     });
   }
+
+  async onRepair(session: PairSession, device: any) {
+    this.log('starting repair');
+    let routerIP = this.homey.settings.get('ip');
+    let username = this.homey.settings.get('username');
+    let password = this.homey.settings.get('password');
+
+    session.setHandler('router_ip_confirmed', async (routerIPFromView) => {
+      this.log('pair: router_ip_confirmed');
+      routerIP = routerIPFromView;
+      this.log(routerIP);
+      const ipRegex = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gi;
+      if (ipRegex.test(routerIP)) {
+        routerIP = 'http://' + routerIP;
+        this.log(routerIP);
+        return true;
+      } else {
+        this.log('invalid ip provided');
+        return false;
+      }
+    });
+
+    session.setHandler('login', async (data: {username: string, password: string}) => {
+      this.log('pair: login');
+      username = data.username.trim();
+      password = data.password;
+      this.log('creating client');
+      try {
+        let client = new AsusWRT(routerIP, username, password);
+        const routers = await client.getRouters();
+        client.dispose();
+        if (routers.length === 0) {
+          this.log('failed to login');
+          return Promise.reject(Error('Failed to login'));
+        } else {
+          this.homey.settings.set("ip", routerIP);
+          this.homey.settings.set("username", username);
+          this.homey.settings.set("password", password);
+          return true;
+        }
+      } catch {
+        return Promise.reject(Error('Failed to login'));
+      }
+    });
+  }
+
 
   private getIcon(productId: string): string {
     const supportedIcons = [
